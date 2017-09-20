db6049b79fcc84612435e18937d3a7218ef7fe4c
Eric Busboom <eric@clarinova.com>
2017-09-20 08:22:00 -0700

    Fixing setup problems

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='appurl',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/CivicKnowledge/appurl',
     license='MIT',
     author='Eric Busboom',
