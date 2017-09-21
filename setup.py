72c5409797b8945010a360480b959c453e074059
Eric Busboom <eric@clarinova.com>
2017-09-21 09:06:36 -0700

    Rev version and publish

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='appurl',
-    version='0.1.6',
+    version='0.1.7',
     url='https://github.com/CivicKnowledge/appurl',
     license='MIT',
     author='Eric Busboom',
