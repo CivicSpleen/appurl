b0e0bf452a29e99c74776d6447f4f9ede34ebd2f
Eric Busboom <eric@civicknowledge.com>
2017-11-17 20:05:42 -0800

    Reformat code

--- appurl/test/test_parse_url.py
+++ appurl/test/test_parse_url.py
@@ -6,14 +6,10 @@ from appurl.url import parse_app_url
 from appurl.web.download import Downloader
 
 
-
-
 class TestUrlParse(unittest.TestCase):
-
     def test_fragment(self):
-
         def u(frag):
-            return parse_app_url("http://example.com/foo.csv#"+frag)
+            return parse_app_url("http://example.com/foo.csv#" + frag)
 
         self.assertEqual({}, u('a').fragment_query)
         self.assertEqual({}, u('a;b').fragment_query)
@@ -30,7 +26,6 @@ class TestUrlParse(unittest.TestCase):
         url.encoding = 'ascii'
         url.start = 5
 
-
         self.assertEqual('http://example.com/foo.csv#a;b&encoding=ascii&start=5', str(url))
 
         url = u('a;b&target_format=foo&resource_format=bar')
@@ -38,17 +33,15 @@ class TestUrlParse(unittest.TestCase):
         self.assertEqual('foo', url.target_format)
         self.assertEqual('bar', url.resource_format)
 
-
         us = 'http://public.source.civicknowledge.com/example.com/sources/test_data.zip#unicode-latin1.csv&encoding=latin1'
 
         url = parse_app_url(us)
 
-        self.assertEqual('latin1',url.encoding)
+        self.assertEqual('latin1', url.encoding)
         self.assertEqual('latin1', url.get_resource().encoding)
         self.assertEqual('latin1', url.get_resource().get_target().encoding)
 
     def test_fragment_2(self):
-
         url = parse_app_url(
             'http://public.source.civicknowledge.com/example.com/sources/renter_cost_excel07.zip#target_format=xlsx')
 
@@ -59,7 +52,7 @@ class TestUrlParse(unittest.TestCase):
         r = url.get_resource()
 
         self.assertEqual('xlsx', r.target_format)
-        self.assertEqual('zip',r.resource_format)
+        self.assertEqual('zip', r.resource_format)
 
     def test_two_extensions(self):
         u_s = 'http://public.source.civicknowledge.com/example.com/sources/simple-example.csv.zip'
@@ -75,7 +68,6 @@ class TestUrlParse(unittest.TestCase):
         self.assertEqual('simple-example.csv', r.target_file)
         self.assertEqual('csv', r.target_format)
 
-
     def test_shapefile_url(self):
         from appurl.web.web import WebUrl
 
@@ -90,7 +82,6 @@ class TestUrlParse(unittest.TestCase):
         self.assertTrue(str(r).startswith('shapefile+'))
 
     def test_s3_url(self):
-
         from appurl.web.s3 import S3Url
 
         url_str = 's3://bucket/a/b/c/file.csv'
@@ -99,11 +90,7 @@ class TestUrlParse(unittest.TestCase):
 
         self.assertIsInstance(u, S3Url)
 
-
-
-
     def test_python_url(self):
-
         from appurl.file import python
         from rowgenerators import get_generator
         from types import ModuleType
@@ -128,6 +115,7 @@ class TestUrlParse(unittest.TestCase):
         g = get_generator(u)
 
         self.assertEqual(45, sum(list(g)))
+
 
 if __name__ == '__main__':
     unittest.main()
